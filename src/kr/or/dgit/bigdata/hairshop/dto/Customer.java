fd9b6aafbbe7b8d6c33471e4c37084f88bad46e2
rohgoon <rohgoon@PTAHKA>
2016-12-25 17:09:58 +0900

    Customer에 cPhone 추가 한것 반영

--- src/kr/or/dgit/bigdata/hairshop/dto/Customer.java
+++ src/kr/or/dgit/bigdata/hairshop/dto/Customer.java
@@ -7,6 +7,12 @@ public class Customer {
 	private String cName;
 	private Date cDob;
 	private Date cDoJoin;
+	private int cPhone;
+	
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+	
 	public Customer(int cNo) {
 		super();
 		this.cNo = cNo;
@@ -24,12 +30,15 @@ public class Customer {
 		this.cDob = cDob;
 	}
 
-	public Customer(int cNo, String cName, Date cDob, Date cDoJoin) {
+	
+
+	public Customer(int cNo, String cName, Date cDob, Date cDoJoin, int cPhone) {
 		super();
 		this.cNo = cNo;
 		this.cName = cName;
 		this.cDob = cDob;
 		this.cDoJoin = cDoJoin;
+		this.cPhone = cPhone;
 	}
 
 	public int getcNo() {
@@ -64,10 +73,63 @@ public class Customer {
 		this.cDoJoin = cDoJoin;
 	}
 
+	public int getcPhone() {
+		return cPhone;
+	}
+
+	public void setcPhone(int cPhone) {
+		this.cPhone = cPhone;
+	}
+
 	@Override
 	public String toString() {
-		return "Customer [cNo=" + cNo + ", cName=" + cName + ", cDob=" + cDob + ", cDoJoin=" + cDoJoin + "]";
+		return "Customer [cNo=" + cNo + ", cName=" + cName + ", cDob=" + cDob + ", cDoJoin=" + cDoJoin + ", cPhone="
+				+ cPhone + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cDoJoin == null) ? 0 : cDoJoin.hashCode());
+		result = prime * result + ((cDob == null) ? 0 : cDob.hashCode());
+		result = prime * result + ((cName == null) ? 0 : cName.hashCode());
+		result = prime * result + cNo;
+		result = prime * result + cPhone;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (cDoJoin == null) {
+			if (other.cDoJoin != null)
+				return false;
+		} else if (!cDoJoin.equals(other.cDoJoin))
+			return false;
+		if (cDob == null) {
+			if (other.cDob != null)
+				return false;
+		} else if (!cDob.equals(other.cDob))
+			return false;
+		if (cName == null) {
+			if (other.cName != null)
+				return false;
+		} else if (!cName.equals(other.cName))
+			return false;
+		if (cNo != other.cNo)
+			return false;
+		if (cPhone != other.cPhone)
+			return false;
+		return true;
+	}
+
 	
 	
 	
